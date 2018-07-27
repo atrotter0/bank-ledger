cd58b40e2436e250bc1aefe869261a41a963377c
Abel Trotter <a_trotter0@hotmail.com>
2018-07-26 23:30:33 -0700

    Add UserAccount properties, add ClearAll method, add tests

--- BankLedger.Tests/ModelTests/UserAccount.Tests.cs
+++ BankLedger.Tests/ModelTests/UserAccount.Tests.cs
@@ -6,8 +6,30 @@ using BankLedger.Models;
 namespace BankLedger.Tests
 {
     [TestClass]
-    public class UserAccountTest
+    public class UserAccountTest : IDisposable
     {
+        public void Dispose()
+        {
+            UserAccount.ClearAll();
+        }
 
+        [TestMethod]
+        public void GetSetProperties_GetsSetsProperties_True()
+        {
+            UserAccount account = new UserAccount("frodo", "123");
+            account.Username = "sam";
+            account.Password = "1234";
+            Assert.AreEqual("sam", account.Username);
+            Assert.AreEqual("1234", account.Password);
+        }
+
+        [TestMethod]
+        public void GetSetAccount_GetsSetsAccounts_True()
+        {
+            UserAccount account = new UserAccount("frodo", "123");
+            Dictionary<string, string> expectedAccounts = new Dictionary<string, string>() {};
+            expectedAccounts.Add(account.Username, account.Password);
+            CollectionAssert.AreEqual(expectedAccounts, UserAccount.Accounts);
+        }
     }
 }
--- BankLedger/Models/UserAccount.cs
+++ BankLedger/Models/UserAccount.cs
@@ -6,6 +6,20 @@ namespace BankLedger.Models
 {
     public class UserAccount
     {
-        
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public static Dictionary<string, string> Accounts = new Dictionary<string, string>() {};
+
+        public UserAccount(string username, string password)
+        {
+            Username = username;
+            Password = password;
+            Accounts.Add(this.Username, this.Password);
+        }
+
+        public static void ClearAll()
+        {
+            Accounts.Clear();
+        }
     }
 }
