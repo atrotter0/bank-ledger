ba4e56054998b7be93b1906271feeededed9f892
Abel Trotter <a_trotter0@hotmail.com>
2018-07-28 13:25:19 -0700

    Adjust indentation

--- BankLedger/wwwroot/js/deposit-withdraw.js
+++ BankLedger/wwwroot/js/deposit-withdraw.js
@@ -1,16 +1,16 @@
 function clearInput(element) {
-	$(element).val("");
+  $(element).val("");
 }
 
 function runDeposit(amount) {
-	$.ajax({
+  $.ajax({
     type: "POST",
     data: { amount: amount },
     url: '/account/deposit/' + amount,
     success: function(result) {
-			console.log(result);
-			roundedAmount = roundTwoDecimals(result.amount);
-			displayBalanceAlert(amount, "deposited");
+      console.log(result);
+      roundedAmount = roundTwoDecimals(result.amount);
+      displayBalanceAlert(amount, "deposited");
       updateBalance(roundedAmount);
     },
     error: function(err) {
@@ -20,23 +20,23 @@ function runDeposit(amount) {
 }
 
 function displayBalanceAlert(amount, depositOrWithdraw) {
-	$("#alert-balance-type").text(depositOrWithdraw);
-	$("#alert-balance-amount").text(amount);
-	$(".alert-update-balance").fadeIn(1200).delay(3000).fadeOut(1200);
+  $("#alert-balance-type").text(depositOrWithdraw);
+  $("#alert-balance-amount").text(amount);
+  $(".alert-update-balance").fadeIn(1200).delay(3000).fadeOut(1200);
 }
 
 function updateBalance(amount) {
-	$("#balance-result").text(amount);
+  $("#balance-result").text(amount);
 }
 
 function roundTwoDecimals(number) {
-	return Math.round(number * 100) / 100;
+  return Math.round(number * 100) / 100;
 }
 
 $(document).ready(function() {
   $("#make-deposit").click(function() {
-		var amount = parseFloat($("#deposit-amount").val());
-		clearInput("#deposit-amount");
-		runDeposit(amount);
-	});
+    var amount = parseFloat($("#deposit-amount").val());
+    clearInput("#deposit-amount");
+    runDeposit(amount);
+  });
 });
